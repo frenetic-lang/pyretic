d30758dcb4590c77f86e87b04998d6768996a603
Joshua Reich <jreich@cs.princeton.edu>
2012-12-25 04:07:11 -0800

    fixed bfs after API change, added print statements

--- virttopos/bfs.py
+++ virttopos/bfs.py
@@ -49,4 +49,10 @@ def setup_virtual_network(network):
             vn.physical_policy = network.flood
             vn.from_maps(vmap)
             vn.topology = vtopo
+            print "------------ underlying network ---------------"
+            print "switches = %s" % topo.nodes(data=True)
+            print "links =    %s" % topo.edges(data=True)
+            print "------------ abstracted network ---------------"
+            print "switches = %s" % vtopo.nodes(data=True)
+            print "links =    %s" % vtopo.edges(data=True)
     return vn
