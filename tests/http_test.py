e8c4d4ccc58ea48d743964d62f66ca160753f193
Joshua Reich <jreich@cs.princeton.edu>
2013-01-17 16:14:25 -0800

    Added a simple http tester. It appears there's some bug either in pyretic or pox though as the download never completes when using our controller (works fine w/ reference controller)

---
+++ tests/http_test.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+"Monitor multiple hosts using popen()/pmonitor()"
+
+from mininet.net import Mininet
+from mininet.topo import SingleSwitchTopo
+from mininet.node import Host, OVSKernelSwitch, RemoteController
+from mininet.util import pmonitor
+from mininet.cli import CLI
+from mininet.clean import cleanup
+from mininet.log import lg
+from time import time, sleep
+from signal import SIGINT
+from subprocess import call, PIPE, STDOUT
+
+def httpTest( N=2 ):
+    "Run pings and monitor multiple hosts using pmonitor"
+
+    ## SET LOGGING AND CLEANUP PREVIOUS MININET STATE, IF ANY
+    lg.setLogLevel('info')
+    cleanup()
+
+    ## INSTEAD OF RUNNING PYRETIC HUB, UNCOMMENT LINE 
+    ## TO SEE THAT THIS WORKS FINE WHEN RUNNING REFERENCE CONTROLLER
+#    call('controller ptcp: &', shell=True)
+
+    ## SET UP TOPOLOGY
+    topo = SingleSwitchTopo( N )
+
+    ## SET UP MININET INSTANCE AND START
+    net = Mininet( topo, switch=OVSKernelSwitch, host=Host, controller=RemoteController )
+    net.start()
+
+    print "Starting test..."
+
+    ## GET THE HOST AND SERVER NAMES
+    hosts = net.hosts
+    client = hosts[ 0 ]
+    server = hosts[ 1 ]
+    
+    ## DICTS FOR USE W/ PMONITOR
+    spopens = {}
+    cpopens = {}
+
+    ## WARMUP SERVER 
+    spopens[server] = server.popen('python', '-m', 'SimpleHTTPServer', '80')
+    sleep(1)
+
+    ## CLIENT REQUEST
+    cpopens[client] = client.popen('wget', '-O', '-', server.IP(), stdout=PIPE, stderr=STDOUT)
+
+    ## MONITOR OUTPUT
+    for h, line in pmonitor( cpopens, timeoutms=5000 ):
+        if h and line:
+            print '%s: %s' % ( h.name, line ),
+
+    ## TO USE THE COMMAND LINE INTERFACE, BEFORE FINISHING, UNCOMMENT
+#    CLI( net )
+
+    ## SHUTDOWN SERVER
+    spopens[server].send_signal( SIGINT )
+    
+    ## SHUTDOWN MININET
+    net.stop()
+
+if __name__ == '__main__':
+    httpTest()
