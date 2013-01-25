bae28026be983175cbf3743311442b73d895ef47
Joshua Reich <jreich@cs.princeton.edu>
2013-01-25 05:07:30 -0800

    updated hub_and_monitor example to non-dyn style

--- examples/hub_and_monitor.py
+++ examples/hub_and_monitor.py
@@ -1,11 +1,11 @@
 
 from frenetic.lib import *
 
-from examples import hub
+from examples.hub import hub
 from examples.monitor_packets import monitor_packets
 
 def hub_and_monitor(network):
-    run(network.install_policy_func(hub.hub), Network.fork(network))
+    run(hub, Network.fork(network))
     run(monitor_packets, Network.fork(network))
     
 main = hub_and_monitor
