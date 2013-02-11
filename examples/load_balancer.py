39dfcb785e3ced3a92925d57ca6570878a95b9de
Joshua Reich <jreich@cs.princeton.edu>
2013-02-11 02:54:58 -0500

    comments to load balancer

--- examples/load_balancer.py
+++ examples/load_balancer.py
@@ -114,8 +114,9 @@ def main(clients, servers):
 #    return static_lb(public_ip,R,H) >> dynamic(learn)()     ## TEST ABOVE WORKS
 #    return dynamic(lb)(public_ip,R,H) >> dynamic(learn)()  ## TEST ABOVE DOESN'T WORK (B/C HOST AND REPLICAS ARE ON SAME LAN, SO USE OLD ARPS ON REBALANCE, CODE WORKS, BUT TEST SETUP NEEDS WORK)
 
-    return virtualize(static_lb(public_ip,R,H) >> dynamic(learn)(), BFS())
+#    return virtualize(static_lb(public_ip,R,H) >> dynamic(learn)(), BFS())  # OR VIRTUALIZE ON BFS
 
-### DON'T WORK
+
+### THESE DON'T WORK (YET)
 #    return virtualize(static_lb(public_ip,R,H), BFS()) >> dynamic(learn)()
 #    return virtualize(dynamic(lb)(public_ip,R,H), BFS()) >> dynamic(learn)()
