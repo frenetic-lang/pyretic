94879201527ed26c5087efe9d1f3a562ebddee91
Joshua Reich <jreich@cs.princeton.edu>
2013-02-11 15:02:56 -0500

    cleanup

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -4,4 +4,4 @@
 ~/pyretic/tests/learning_switch_tester.py
 ~/pyretic/tests/load_balancer_tester.py
 ~/pyretic/tests/learning_switch_on_bfs_tester.py
-#~/pyretic/tests/learning_switch_on_spanning_tree_tester.py
+~/pyretic/tests/learning_switch_on_spanning_tree_tester.py
