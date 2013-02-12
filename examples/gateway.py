377df1d3f67cf3b6f787bc5371bc4c348fd31b7e
Joshua Reich <jreich@cs.princeton.edu>
2013-02-12 16:24:22 -0500

    fixed bug in gateway where linked_to was being set to wrong type (was Port, should have been Location

--- examples/gateway.py
+++ examples/gateway.py
@@ -103,33 +103,33 @@ class GatewayVirt(Virtualizer):
                 
                 vtopo.add_edge(3, 1000, {3: 3, 1000: 1})
 
-                vtopo.node[3]['ports'][3].linked_to = vtopo.node[1000]['ports'][1]
-                vtopo.node[1000]['ports'][1].linked_to = vtopo.node[3]['ports'][3]
+                vtopo.node[3]['ports'][3].linked_to = Location(1000,1)
+                vtopo.node[1000]['ports'][1].linked_to = Location(3,3)
                 
                 vtopo.add_edge(4, 1000, {4: 2, 1000: 2})
 
-                vtopo.node[4]['ports'][2].linked_to = vtopo.node[1000]['ports'][2]
-                vtopo.node[1000]['ports'][2].linked_to = vtopo.node[4]['ports'][2]
+                vtopo.node[4]['ports'][2].linked_to = Location(1000,2)
+                vtopo.node[1000]['ports'][2].linked_to = Location(4,2)
                 
                 vtopo.add_edge(1000, 1001, {1000: 3, 1001: 1})
                 
-                vtopo.node[1000]['ports'][3].linked_to = vtopo.node[1001]['ports'][1]
-                vtopo.node[1001]['ports'][1].linked_to = vtopo.node[1000]['ports'][3]
+                vtopo.node[1000]['ports'][3].linked_to = Location(1001,1)
+                vtopo.node[1001]['ports'][1].linked_to = Location(1000,3)
                 
                 vtopo.add_edge(1001, 1002, {1001: 2, 1002: 3})
                 
-                vtopo.node[1001]['ports'][2].linked_to = vtopo.node[1002]['ports'][3]
-                vtopo.node[1002]['ports'][3].linked_to = vtopo.node[1001]['ports'][2]
+                vtopo.node[1001]['ports'][2].linked_to = Location(1002,3)
+                vtopo.node[1002]['ports'][3].linked_to = Location(1001,2)
                 
                 vtopo.add_edge(5, 1002, {5: 2, 1002: 1})
 
-                vtopo.node[5]['ports'][2].linked_to = vtopo.node[1002]['ports'][1]
-                vtopo.node[1002]['ports'][1].linked_to = vtopo.node[5]['ports'][2]
+                vtopo.node[5]['ports'][2].linked_to = Location(1002,1)
+                vtopo.node[1002]['ports'][1].linked_to = Location(5,2)
                 
                 vtopo.add_edge(7, 1002, {7: 3, 1002: 2})
 
-                vtopo.node[7]['ports'][3].linked_to = vtopo.node[1002]['ports'][2]
-                vtopo.node[1002]['ports'][2].linked_to = vtopo.node[7]['ports'][3]
+                vtopo.node[7]['ports'][3].linked_to = Location(2002,2)
+                vtopo.node[1002]['ports'][2].linked_to = Location(7,3)
             except:
                 pass
             
