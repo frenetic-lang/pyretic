184efe6abebe5c909090fd788b5a85436ff1f369
Joshua Reich <jreich@cs.princeton.edu>
2013-05-13 23:15:49 -0400

    Added example of using aggregate queries

---
+++ pyretic/examples/aggregate_queries.py
@@ -0,0 +1,56 @@
+
+################################################################################
+# The Pyretic Project                                                          #
+# frenetic-lang.org/pyretic                                                    #
+# author: Joshua Reich (jreich@cs.princeton.edu)                               #
+################################################################################
+# Licensed to the Pyretic Project by one or more contributors. See the         #
+# NOTICES file distributed with this work for additional information           #
+# regarding copyright and ownership. The Pyretic Project licenses this         #
+# file to you under the following license.                                     #
+#                                                                              #
+# Redistribution and use in source and binary forms, with or without           #
+# modification, are permitted provided the following conditions are met:       #
+# - Redistributions of source code must retain the above copyright             #
+#   notice, this list of conditions and the following disclaimer.              #
+# - Redistributions in binary form must reproduce the above copyright          #
+#   notice, this list of conditions and the following disclaimer in            #
+#   the documentation or other materials provided with the distribution.       #
+# - The names of the copyright holds and contributors may not be used to       #
+#   endorse or promote products derived from this work without specific        #
+#   prior written permission.                                                  #
+#                                                                              #
+# Unless required by applicable law or agreed to in writing, software          #
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT    #
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the     #
+# LICENSE file distributed with this work for specific language governing      #
+# permissions and limitations under the License.                               #
+################################################################################
+
+from pyretic.lib.corelib import *
+from pyretic.lib.std import *
+from pyretic.modules.mac_learner import mac_learner
+
+def count_printer(counts):
+    print "----counts------"
+    print counts
+
+def packet_counts():
+  q = counts(1,['srcip','dstip'])
+  q.register_callback(count_printer)
+  return q
+
+def size_printer(sizes):
+    print "----sizes------"
+    print sizes
+
+def packet_sizes():
+  q = sizes(1,['srcip','dstip'])
+  q.register_callback(size_printer)
+  return q
+
+
+### Main ###
+
+def main():
+    return packet_counts() | packet_sizes() | mac_learner()
