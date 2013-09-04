74335261b8f84fdac1554d62e06dc631e49b4216
Joshua Reich <jreich@cs.princeton.edu>
2013-09-03 22:23:32 -0400

    added rewrite example on which proactive compilation is currently broken (though whether at the OVS switch, of_client, etc. is still unclear)

---
+++ pyretic/examples/rewrite.py
@@ -0,0 +1,70 @@
+
+################################################################################
+# The Pyretic Project                                                          #
+# frenetic-lang.org/pyretic                                                    #
+# author: Joshua Reich (jreich@cs.princeton.edu)                               #
+################################################################################
+# Licensed to the Pyretic Project by one or more contributors. See the         #
+# NOTICES file distributed with this work for additional information           #
+# regarding copyright and ownership. The Pyretic Project licenses this         #
+# file to you under the following license.                                     #
+#                                                                              #
+# Redistribution and use in source and binary forms, with or without           #
+# modification, are permitted provided the following conditions are met:       #
+# - Redistributions of source code must retain the above copyright             #
+#   notice, this list of conditions and the following disclaimer.              #
+# - Redistributions in binary form must reproduce the above copyright          #
+#   notice, this list of conditions and the following disclaimer in            #
+#   the documentation or other materials provided with the distribution.       #
+# - The names of the copyright holds and contributors may not be used to       #
+#   endorse or promote products derived from this work without specific        #
+#   prior written permission.                                                  #
+#                                                                              #
+# Unless required by applicable law or agreed to in writing, software          #
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT    #
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the     #
+# LICENSE file distributed with this work for specific language governing      #
+# permissions and limitations under the License.                               #
+################################################################################
+
+
+################################################################################
+# SETUP                                                                        #
+# -------------------------------------------------------------------          #
+# mininet:  mininet.sh --topo clique,4,4 (or other single subnet)              #
+################################################################################
+
+from pyretic.lib.corelib import *
+from pyretic.lib.std import *
+from pyretic.lib.query import *
+
+mac1 = EthAddr('00:00:00:00:00:01')
+mac2 = EthAddr('00:00:00:00:00:02')
+mac3 = EthAddr('00:00:00:00:00:03')
+macB = EthAddr('FF:FF:FF:FF:FF:FF')
+ip1 = IPAddr('10.0.0.1')
+ip2 = IPAddr('10.0.0.2')
+ip3 = IPAddr('10.0.0.3')
+p = IPAddr('10.0.0.11')
+
+modify = if_(match(srcip=ip1),
+             modify(srcip=p),
+             if_(match(dstip=p),
+                 modify(dstip=ip1)))
+              
+l2route = (((match(dstmac=mac1) | match(dstmac=macB)) >> fwd(1)) +
+           ((match(dstmac=mac2) | match(dstmac=macB)) >> fwd(2)) +
+           ((match(dstmac=mac3) | match(dstmac=macB)) >> fwd(3)) )
+
+l3route = ((match(dstip=ip1) >> fwd(1)) +
+           (match(dstip=ip2) >> fwd(2)) +
+           (match(dstip=ip3) >> fwd(3)) )
+
+policy = modify >> l2route
+
+def main():
+    return policy
+
+
+
+
