c90724696b5e47fad8623609caaa5c9d5dcd243b
Joshua Reich <jreich@cs.princeton.edu>
2013-10-21 14:59:49 -0400

    comment out output processing from of_client given that output is currently sent to stdout

--- pyretic.py
+++ pyretic.py
@@ -46,13 +46,13 @@ of_client = None
 
 def signal_handler(signal, frame):
     print '\n----starting pyretic shutdown------'
-#    for thread in threading.enumerate():
-#        print (thread,thread.isAlive())
+    # for thread in threading.enumerate():
+    #     print (thread,thread.isAlive())
     print "attempting to kill of_client"
     of_client.kill()
-    print "attempting get output of of_client:"
-    output = of_client.communicate()[0]
-    print output
+    # print "attempting get output of of_client:"
+    # output = of_client.communicate()[0]
+    # print output
     print "pyretic.py done"
     sys.exit(0)
 
