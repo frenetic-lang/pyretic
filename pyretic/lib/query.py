a1c9f38cccb6610a7e61f3e39ee1df5903d2fd75
Srinivas Narayana <ngsrinivas@gmail.com>
2014-01-10 16:00:31 -0800

    wip: first shot at counts() query to build grouped countbuckets

--- pyretic/lib/query.py
+++ pyretic/lib/query.py
@@ -45,12 +45,15 @@ class LimitFilter(DynamicFilter):
         self.done = []
         super(LimitFilter,self).__init__(identity)
 
-    def update_policy(self,pkt):
+    def get_pred_from_pkt(self, pkt):
         if self.group_by:    # MATCH ON PROVIDED GROUP_BY
             pred = match([(field,pkt[field]) for field in self.group_by])
         else:              # OTHERWISE, MATCH ON ALL AVAILABLE GROUP_BY
             pred = match([(field,pkt[field]) 
                               for field in pkt.available_group_by()])
+
+    def update_policy(self,pkt):
+        pred = self.get_pred_from_pkt(pkt)
         # INCREMENT THE NUMBER OF TIMES MATCHING PKT SEEN
         try:
             self.seen[pred] += 1
@@ -86,6 +89,61 @@ class packets(DerivedPolicy):
         
     def __repr__(self):
         return "packets\n%s" % repr(self.policy)
+
+class counts(DynamicPolicy):
+    """A CountBucket that returns distinct counts per grouping, defined by a set
+    of header fields.
+
+    :param interval: time period between successive pulls of switch statistics
+    :type interval: some float
+    :param group_by: list of grouping fields
+    :type group_by: string list
+    """
+    def __init__(self, interval=None, group_by=[]):
+        self.callbacks = []
+        self.bucket_policies = []
+        self.seen_preds = []
+        self.queried_preds = []
+        self.groupby_filter = LimitFilter(1,group_by)
+        self.fb = FwdBucket() # fb sees first packet of each new grouping
+        self.fb.register_callback(self.groupby_filter.update_policy)
+        self.fb.register_callback(self.init_countbucket)
+        super(counts,self).__init__(self.groupby_filter >> self.fb)
+
+    def init_countbucket(self, pkt):
+        """When a packet from a previously unseen group arrives, set up new
+        count buckets for the same.
+        """
+        pred = self.groupby_filter.get_pred_from_pkt(pkt)
+        cb = CountBucket()
+        cb.register_callback(self.collect_pred(pred))
+        self.bucket_policies.append(pred >> cb)
+        self.seen_preds.append(pred)
+        self.policy = ((self.groupby_filter >> self.fb) +
+                       union(self.bucket_policies))
+
+    def collect_pred(self, pred):
+        """Return a callback function specific to each grouping predicate."""
+        def collect(pkt_byte_counts):
+            if pred in self.queried_preds:
+                self.returned_counts[pred] = pkt_byte_counts
+                self.queried_preds.remove(pred)
+            if not self.queried_preds:
+                for f in self.callbacks:
+                    f(self.returned_counts)
+        return collect
+
+    def register_callback(self, fn):
+        self.callbacks.append(fn)
+
+    def pull_stats(self):
+        print "to do"
+
+    def collect_buckets(self):
+        print "to do"
+
+    def __repr__(self):
+        return "counts\n%s" % repr(self.policy)
 
 
 class AggregateFwdBucket(FwdBucket):
