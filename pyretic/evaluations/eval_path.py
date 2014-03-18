4f9c8b4b7f2b7712b142100d8f140e460eaf4973
Srinivas Narayana <ngsrinivas@gmail.com>
2014-03-18 14:01:54 -0700

    traffic matrix query and cyclic forwarding policy

--- pyretic/evaluations/eval_path.py
+++ pyretic/evaluations/eval_path.py
@@ -39,15 +39,41 @@ from pyretic.lib.std import *
 from pyretic.modules.mac_learner import mac_learner
 from pyretic.lib.path import *
 from pyretic.lib.query import counts
+import threading
 
-def query_callback(test_num):
+def query_callback(id_str):
     def actual_callback(pkt):
         print '**************'
         print datetime.now()
-        print 'Test', test_num, ' -- got a callback from installed path query!'
+        print 'Test', id_str, ' -- got a callback from installed path query!'
         print pkt
         print '**************'
     return actual_callback
+
+def cycle_forwarding_policy(n):
+    """The correct forwarding policy for a cycle topology. n is the number of
+    nodes in the cycle."""
+    ip_prefix = '10.0.0.'
+    h1_ip = IP(ip_prefix + str(1))
+    f = ((match(dstip=h1_ip) >> match(switch=1) >> fwd(3)) +
+         (match(dstip=h1_ip) >> (~match(switch=1)) >> fwd(2)))
+    for h in range(2,n+1):
+        h_ip = IP(ip_prefix + str(h))
+        f += (match(dstip=h_ip) >> ((match(switch=h) >> fwd(3)) +
+                                    (match(switch=1) >> fwd(1)) +
+                                    (((~match(switch=1))&(~match(switch=h))) >>
+                                     fwd(2))))
+    return f
+
+def query_func(bucket, interval, id_str):
+    """Canonical callback function for a countbucket."""
+    while True:
+        output = str(datetime.now())
+        output += " Pulling stats for bucket " + id_str
+        # output += bucket.get_matches()
+        print output
+        bucket.pull_stats()
+        time.sleep(interval)
 
 ## Test: Loop Forwarding
 def path_test_loop(**kwargs):
@@ -59,6 +85,7 @@ def path_test_loop(**kwargs):
     plist = []
     assert 'n' in kwargs
     n = int(kwargs['n'])
+    # actually this needs to change and include id* on both sides
     for s in range(1,n+1):
         p = (atom(match(switch=s)) ^ +(atom(~match(switch=s))) ^
              atom(match(switch=s)))
@@ -76,41 +103,61 @@ def loop_fwding(**kwargs):
     """
     assert 'n' in kwargs
     n = int(kwargs['n'])
+    f = cycle_forwarding_policy(n)
     ip_prefix = '10.0.0.'
     h1_ip = IP(ip_prefix + str(1))
     h3_ip = IP(ip_prefix + str(3))
-    f = ((match(dstip=h1_ip) >> match(switch=1) >> (~match(srcip=h3_ip)) >>
-          fwd(3)) +
-         (match(dstip=h1_ip) >> match(switch=1) >> match(srcip=h3_ip) >>
-          fwd(2)) + # explicit loop added between h2 --> h1
-         (match(dstip=h1_ip) >> (~match(switch=1)) >> fwd(2)))
-    for h in range(2,n+1):
-        h_ip = IP(ip_prefix + str(h))
-        f += (match(dstip=h_ip) >> ((match(switch=h) >> fwd(3)) +
-                                    (match(switch=1) >> fwd(1)) +
-                                    (((~match(switch=1))&(~match(switch=h))) >>
-                                     fwd(2))))
+    f += (match(srcip=h3_ip, dstip=h1_ip, switch=1) >> fwd(2)) # h1 <--> h4 loop
     return f
 
 ## Test: Traffic Matrix
-def path_test_tm():
-    p = []
-    n = 5
+def path_test_tm(**kwargs):
+    """ Query that measures the traffic matrix. """
+    default_timeout_value = 10 # poll timeout for traffic matrix counters
+
+    def query_thread_setup(p, timeout, id_str):
+        cb = CountBucket()
+        p.set_bucket(cb)
+        p.register_callback(query_callback(id_str))
+        query_thread = threading.Thread(target=query_func, args=(cb, timeout, id_str))
+        query_thread.daemon = True
+        query_thread.start()
+
+    def create_id_string(s1, s2):
+        return str(s1) + '--->' + str(s2)
+
+    def get_timeout(args):
+        if 'poll' in args:
+            return int(args['poll'])
+        else:
+            return default_timeout_value
+
+    plist = []
+    n = int(kwargs['n'])
+    timeout = get_timeout(kwargs)
     for s1 in range(1,n+1):
         for s2 in range(s1+1,n+1):
-            p += [atom(ingress_filter() & match(switch=s1)) ^
-                  end_path(match(switch=s2))]
-    return p
+            p = (atom(ingress_network() & match(switch=s1)) ^
+                 end_path(match(switch=s2)))
+            query_thread_setup(p, timeout, create_id_string(s1, s2))
+            plist.append(p)
+            p = (atom(ingress_network() & match(switch=s2)) ^
+                 end_path(match(switch=s1)))
+            query_thread_setup(p, timeout, create_id_string(s2, s1))
+            plist.append(p)
+    return plist
 
-def tm_fwding():
-    return mac_learner()
+def tm_fwding(**kwargs):
+    assert 'n' in kwargs
+    n = int(kwargs['n'])
+    return cycle_forwarding_policy(n)
 
 # List of tests, to be used with the --test= parameter.
 test_mains = {'loops': loop_fwding,
-              'traffic_matrix': tm_fwding }
+              'tm': tm_fwding }
 
 test_path_mains = {'loops': path_test_loop,
-                   'traffic_matrix': path_test_tm }
+                   'tm': path_test_tm }
 
 def test_setup(test_dict, default_test, **kwargs):
     params = dict(**kwargs)
