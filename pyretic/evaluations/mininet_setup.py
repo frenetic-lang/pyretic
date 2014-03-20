1f80bd0a897b86ebbf518bf85b47ab85a2ce2dde
Srinivas Narayana <ngsrinivas@gmail.com>
2014-03-20 15:29:56 -0700

    function/variable naming style change

--- pyretic/evaluations/mininet_setup.py
+++ pyretic/evaluations/mininet_setup.py
@@ -8,14 +8,15 @@ from extratopos import *
 import subprocess
 
 num_hosts = 5
+listen_port = 6634
 
-def mnCleanup():
+def mn_cleanup():
     subprocess.call("sudo mn -c", shell=True)
 
-def SetupTopo():
+def setup_topo():
     return CycleTopo(num_hosts, num_hosts)
 
-def pingFlowPairs(net, hosts_src, hosts_dst):
+def ping_flow_pairs(net, hosts_src, hosts_dst):
     """ Test connectivity between flow sources and destinations """
     assert len(hosts_src) == len(hosts_dst)
     for i in range(0, len(hosts_src)):
@@ -23,41 +24,48 @@ def pingFlowPairs(net, hosts_src, hosts_dst):
         sent, received = net._parsePing(result)
         print ('%d ' % i) if received else 'X '
 
-def getHosts(net):
+def get_hosts(net):
     """ Get a list of host objects from the network object """
     hosts = []
     for i in range(1, num_hosts+1):
         hosts.append(net.getNodeByName('h' + str(i)))
     return hosts
 
-def getSwitches(net):
+def get_switches(net):
     switches = []
     for i in range(1, num_hosts+1):
         switches.append(net.getNodeByName('s' + str(i)))
     return switches
 
-def runIperfTest(net, hosts_src, hosts_dst, switches_list):
+def run_iperf_test(net, hosts_src, hosts_dst, switches_list):
     print "Nothing to do here."
 
-def queryTest():
-    """ Main """
-    mnCleanup()
+def test_switch_rules(switches):
+    print "Nothing done here yet, either."
 
-    topo = SetupTopo()
+def query_test():
+    """ Main """
+    mn_cleanup()
+
+    topo = setup_topo()
     # net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
-    net = Mininet(topo=topo, host=CPULimitedHost, controller=RemoteController, listenPort=6634)
+    net = Mininet(topo=topo, host=CPULimitedHost, controller=RemoteController,
+                  listenPort=listen_port)
     net.start()
 
-    hosts = getHosts(net)
+    hosts = get_hosts(net)
+    switches = get_switches(net)
     hosts_src = hosts
     hosts_dst = hosts
 
     print "Testing network connectivity"
-    pingFlowPairs(net, hosts_src, hosts_dst)
+    ping_flow_pairs(net, hosts_src, hosts_dst)
+
+    print "Testing switch rules"
+    test_switch_rules(switches)
 
     print "Starting iperf tests"
-    switches_list = getSwitches(net)
-    runIperfTest(net, hosts_src, hosts_dst, switches_list)
+    run_iperf_test(net, hosts_src, hosts_dst, switches)
 
     CLI(net)
 
@@ -65,4 +73,4 @@ def queryTest():
 
 if __name__ == "__main__":
     setLogLevel('info')
-    queryTest()
+    query_test()
