097098255916344d4d72eaea3e0be0db44263736
Srinivas Narayana <ngsrinivas@gmail.com>
2014-03-21 20:47:48 -0700

    work in progress: refactoring for test case-based functionality

--- pyretic/evaluations/mininet_setup.py
+++ pyretic/evaluations/mininet_setup.py
@@ -52,32 +52,71 @@ def kill_process(p, process_str):
 #### Test-case-specific functions
 ################################################################################
 
-def setup_cycle_topo(num_hosts):
-    return CycleTopo(num_hosts, num_hosts)
+# First, common functions which call the test-case-specific functions:
+def setup_network(test, global_params, testwise_params):
+    if test == "tm":
+        return setup_tm_network(global_params, testwise_params)
+    else:
+        print "Unknown test case topology!"
+        sys.exit(0)
 
-def get_workload_setup(hosts):
+def setup_full_traffic_measurement(test, global_params, testwise_params,
+                                   switches):
+    if test == "tm":
+        return setup_tm_full_traffic_measurement(global_params, testwise_params,
+                                                 switches)
+    else:
+        print "Unknown test case traffic measurement call!"
+        sys.exit(0)
+
+def setup_workload(test, global_params, testwise_params, hosts):
+    if test == "tm":
+        return setup_tm_workload(hosts)
+    else:
+        print "Unknown test case for workload setup!"
+        sys.exit(0)
+
+# 1. Traffic-matrix-specific setup functions
+def setup_tm_network(global_params, testwise_params):
+    """ Set up a cycle topology of num_hosts. """
+    def get_hosts(net, num_hosts):
+        """ Get a list of host objects from the network object """
+        hosts = []
+        for i in range(1, num_hosts+1):
+            hosts.append(net.getNodeByName('h' + str(i)))
+        return hosts
+
+    def get_switches(net, num_switches):
+        switches = []
+        for i in range(1, num_switches+1):
+            switches.append(net.getNodeByName('s' + str(i)))
+        return switches
+
+    assert 'n' in testwise_params
+    assert 'listen_port' in global_params
+    num_hosts = int(testwise_params['n'])
+    listen_port = int(global_params['listen_port'])
+    topo = CycleTopo(num_hosts, num_hosts)
+    net = Mininet(topo=topo, host=CPULimitedHost, controller=RemoteController,
+                  listenPort=listen_port)
+    net.start()
+    hosts = get_hosts(net, num_hosts)
+    switches = get_switches(net, num_hosts)
+    return (net, hosts, switches)
+
+def setup_tm_workload(hosts):
     hosts_src = hosts
     hosts_dst = hosts[1:] + [hosts[0]]
-    return (hosts_src, hosts_dst)
+    per_flow_bw = ["8M"] * len(hosts)
+    return (hosts_src, hosts_dst, per_flow_bw)
 
-def get_hosts(net, num_hosts):
-    """ Get a list of host objects from the network object """
-    hosts = []
-    for i in range(1, num_hosts+1):
-        hosts.append(net.getNodeByName('h' + str(i)))
-    return hosts
-
-def get_switches(net, num_switches):
-    switches = []
-    for i in range(1, num_switches+1):
-        switches.append(net.getNodeByName('s' + str(i)))
-    return switches
-
-def measure_total_traffic(total_traffic_prefix, test_duration_sec, slack,
-                          switches):
+def setup_tm_full_traffic_measurement(global_params, testwise_params, switches):
     """ Setup tshark collectors and statistics for the 'total' traffic in the
     network.
     """
+    total_traffic_prefix = global_params['total_traffic_prefix']
+    test_duration_sec = global_params['test_duration_sec']
+    slack_factor = global_params['slack_factor']
     out_fds = []
     processes = []
     for s in switches:
@@ -170,27 +209,27 @@ def wait_switch_rules_installed(switches):
     print "Rules fully installed after waiting", time_waited, "seconds"
 
 def run_iperf_test(net, hosts_src, hosts_dst, test_duration_sec,
-                   per_transfer_bandwidth):
+                   per_transfer_bandwidth, client_prefix, server_prefix):
     """Run UDP iperf transfers between hosts_src and hosts_dst pairs for
     test_duration_sec seconds, with a targeted bandwidth of
     per_transfer_bandwidth.
     """
     # start iperf servers
     for dst in hosts_dst:
-        dst_server_file = dst.name + "-server-udp.txt"
+        dst_server_file = dst.name + '-' + server_prefix
         dst.cmd("iperf -u -s -p 5002 -i 5 > " + dst_server_file + " &")
     print "Finished starting up iperf servers..."
 
     # start iperf client transfers
     for i in range(0, len(hosts_src)):
         src = hosts_src[i]
-        src_client_file = src.name + "-client-udp.txt"
+        src_client_file = src.name + '-' + client_prefix
         src.cmd("iperf -t " + str(test_duration_sec) + " -c " +
                 hosts_dst[i].IP() + " -u -p 5002 -i 5 -b " +
                 per_transfer_bandwidth[i] + " > " + src_client_file + "&")
     print "Client transfers initiated."
 
-def set_up_overhead_statistics(overheads_file, test_duration_sec, slack):
+def setup_overhead_statistics(overheads_file, test_duration_sec, slack):
     cmd = ("tshark -q -i lo -z io,stat," + str(slack * test_duration_sec) +
            ",'of.pktin||of.stats_flow_byte_count' -f 'tcp port 6633'")
     f = open(overheads_file, "w")
@@ -205,17 +244,26 @@ def set_up_overhead_statistics(overheads_file, test_duration_sec, slack):
 def query_test():
     """ Main """
     # Configuring the experiment.
-    num_hosts = 5
+    # Global parameters used by specific tests as well
     listen_port = 6634
     test_duration_sec = 30
-    per_flow_bw = ["8M"] * num_hosts
-    overheads_file = "tshark_output.txt"
     slack_factor = 5 # slack for ensuring tshark statistics fall into one interval
-    test = "tm"
-    testwise_params = {'n': str(num_hosts)}
+    total_traffic_prefix = "total-traffic"
+    global_params = { 'listen_port' : listen_port,
+                      'test_duration_sec': test_duration_sec,
+                      'total_traffic_prefix': total_traffic_prefix,
+                      'slack_factor' : slack_factor }
+
+    # Global parameters not used elsewhere outside this function
+    overheads_file = "tshark_output.txt"
     c_out = "pyretic-stdout.txt"
     c_err = "pyretic-stderr.txt"
-    total_traffic_prefix = "total-traffic"
+    iperf_client_prefix = "client-udp.txt"
+    iperf_server_prefix = "server-udp.txt"
+
+    # Specification of testwise parameters (in code, for now)
+    full_testwise_params = { "tm" : {'n': '5'} }
+    test = "tm"
 
     # Hack to set pythonpath.
     pypath = "/home/mininet/pyretic:/home/mininet/mininet:/home/mininet/pox"
@@ -223,32 +271,29 @@ def query_test():
     # Actual experiment setup.
     mn_cleanup()
 
+    testwise_params = full_testwise_params[test]
+
     print "Start pyretic controller"
     ctlr = pyretic_controller(test, testwise_params, c_out, c_err, pypath)
 
     print "Setting up topology"
-    topo = setup_cycle_topo(num_hosts)
-    net = Mininet(topo=topo, host=CPULimitedHost, controller=RemoteController,
-                  listenPort=listen_port)
-    net.start()
-    hosts = get_hosts(net, num_hosts)
-    switches = get_switches(net, num_hosts)
+    (net, hosts, switches) = setup_network(test, global_params, testwise_params)
 
     print "Setting up overhead statistics measurements"
-    tshark = set_up_overhead_statistics(overheads_file, test_duration_sec,
-                                        slack_factor)
+    tshark = setup_overhead_statistics(overheads_file, test_duration_sec,
+                                       slack_factor)
 
     print "Setting up collectors for total traffic"
-    switch_stats = measure_total_traffic(total_traffic_prefix,
-                                         test_duration_sec,
-                                         slack_factor,
-                                         switches)
+    switch_stats = setup_full_traffic_measurement(test, global_params,
+                                                  testwise_params, switches)
 
     print "Setting up handlers for graceful experiment abort"
-    signal.signal(signal.SIGINT, get_abort_handler(ctlr, tshark, switch_stats, net))
+    signal.signal(signal.SIGINT, get_abort_handler(ctlr, tshark, switch_stats,
+                                                   net))
 
     print "Setting up workload configuration"
-    (hosts_src, hosts_dst) = get_workload_setup(hosts)
+    (hosts_src, hosts_dst, per_flow_bw) = setup_workload(test, global_params,
+                                                         testwise_params, hosts)
 
     print "Setting up switch rules"
     wait_switch_rules_installed(switches)
@@ -257,7 +302,8 @@ def query_test():
     # ping_flow_pairs(net, hosts_src, hosts_dst)
 
     print "Starting iperf tests"
-    run_iperf_test(net, hosts_src, hosts_dst, test_duration_sec, per_flow_bw)
+    run_iperf_test(net, hosts_src, hosts_dst, test_duration_sec, per_flow_bw,
+                   iperf_client_prefix, iperf_server_prefix)
 
     print ("Running iperf transfer tests. This may take a while (" +
            str(test_duration_sec) + " seconds)...")
