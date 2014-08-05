4238132b7427545c2c7a55b56efe9bfc1edb8f9c
Joshua Reich <jreich@cs.princeton.edu>
2014-08-05 19:53:42 -0400

    updated topology_printer example

--- pyretic/examples/topology_printer.py
+++ pyretic/examples/topology_printer.py
@@ -12,20 +12,25 @@ from pyretic.lib.std import *
 class topology_printer(DynamicPolicy):
 
     def __init__(self):
-        self.topology = None
+        self.last_topology = None
         self.lock = Lock()
         super(topology_printer,self).__init__()
 
     def set_network(self, network):
         with self.lock:
-            if self.topology and (self.topology == network.topology):
-                pass
+            print '------New Topology Information----------'
+            print '---Switch ID list'
+            print network.switches()
+            print '---Switch ID paired with port ID list'
+            print network.switches(with_ports=True)
+            print '---Convert network topology object to string'
+            print network.topology
+            print '---Has changed: ',
+            if self.last_topology:
+                print self.last_topology != network.topology 
             else:
-                print self.topology
-                print network.topology
-                self.topology = network.topology
-                print self.topology
-
+                print True
+            self.last_topology = network.topology
 
 def main ():
     return topology_printer()
