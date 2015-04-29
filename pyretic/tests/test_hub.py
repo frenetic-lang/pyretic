f66a35e57e63d190d831422fecf380e8989e49aa
Srinivas Narayana <ngsrinivas@gmail.com>
2015-04-29 01:16:41 -0700

    no multitable tests in mininet_tests: it takes time to setup tables

--- pyretic/tests/test_hub.py
+++ pyretic/tests/test_hub.py
@@ -64,8 +64,8 @@ def test_hub_r0(init):
     utils.run_test(test_hub, init.test_dir, init.benchmark_dir, '-m r0')
 def test_hub_p0(init):
     utils.run_test(test_hub, init.test_dir, init.benchmark_dir, '-m p0')
-def test_hub_p0_nx(init):
-    utils.run_test(test_hub, init.test_dir, init.benchmark_dir, '-m p0 --nx')
+# def test_hub_p0_nx(init):
+#     utils.run_test(test_hub, init.test_dir, init.benchmark_dir, '-m p0 --nx')
 
 ### Executing this file starts the mininet instance for this test.
 
