6ff154710a41a91c110b4d4b82bb3b6be21a7089
Srinivas Narayana <ngsrinivas@gmail.com>
2015-07-20 10:44:45 -0700

    add automatic softflowd netflow replay for testing

--- pyretic/tests/test_netflow.py
+++ pyretic/tests/test_netflow.py
@@ -1,7 +1,11 @@
 from pyretic.lib.netflow import *
 from pyretic.core.language import *
-import logging, time
+import logging, time, subprocess, shlex, os, threading
 
+TESTPCAP = "pyretic/scratch/testpcap.pcap"
+
+###################################
+# Test myEval function on packets
 #some sample packets are below.
 p1 = {"header":{"srcmac":"00:0a:95:9d:68:16", "dstmac":"00:0a:95:9d:68:18", "srcip":"192.0.0.1", "dstip":"192.0.0.2", "tos":56, "srcport":8008, "dstport":9900, "ethtype":0x0800, "protocol":"tcp"},"pktcount":40,"bytecount":4000}
 p2 = {"header":{"srcmac":"00:0a:95:9d:78:16", "dstmac":"00:0a:9d:95:68:18", "srcip":"192.0.0.6", "dstip":"192.0.0.9", "tos":38, "srcport":8800, "dstport":9000, "ethtype":0x0800, "protocol":"tcp"},"pktcount":40,"bytecount":3000}
@@ -35,19 +39,48 @@ assert myEval(f10,p1) is p1 and myEval(f10,p2) is p2
 assert myEval(f1,p2) == myEval(f2,p1) == myEval(f3,p1) == myEval(f4,p1) == set()
 assert myEval(f6,p1) == myEval(f7,p2) == myEval(f8,p1) == myEval(f8,p2) == myEval(f9,p1) == set()
 
+###################################
 # Test netflow bucket
 def nf_callback_fn(res):
     print "This is a netflow bucket callback function. I got results:"
     print res
 
+def traffic_thread(loop=True):
+    def run_workload():
+        cmd = "softflowd -n localhost:%d -r %s" % (NFCAPD_PORT, TESTPCAP)
+        try:
+            p = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+        except OSError:
+            print "Errors in replaying traffic workload to netflow collector!"
+            print "Got output:", p
+        except subprocess.CalledProcessError:
+            print "There were errors in running softflowd!"
+            print "Got output:", p
+    ''' Run a do-while-loop of run_workload. '''
+    while True:
+        run_workload()
+        if not loop:
+            break
+        else:
+            time.sleep(7)
+
 logging.basicConfig()
 nb = NetflowBucket()
 nb.register_callback(nf_callback_fn)
+# Check whether multiple nfcapd instances are started, and whether that
+# causes errors by starting different NetflowBuckets. Neither should happen.
 nb.start_nfcapd()
 nb.start_nfcapd()
+nb2 = NetflowBucket()
+
+t = threading.Thread(target=traffic_thread, args=(True,))
+t.daemon = True
+t.start()
+
 try:
     time.sleep(300)
 except KeyboardInterrupt:
     print "Killing netflow test."
+
 nb.kill_nfcapd()
 print "Test complete"
