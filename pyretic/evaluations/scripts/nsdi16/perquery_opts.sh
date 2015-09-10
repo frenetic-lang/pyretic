aab0fe042da15d87a3402a7ce2cf855cc4083758
Srinivas Narayana <ngsrinivas@gmail.com>
2015-09-10 09:59:30 -0700

    refactor eval scripts to share code and run multiple tests at once

--- pyretic/evaluations/scripts/nsdi16/perquery_opts.sh
+++ pyretic/evaluations/scripts/nsdi16/perquery_opts.sh
@@ -1,49 +1,25 @@
 #!/bin/sh
 
+source pyretic/evaluations/scripts/nsdi16/run_tests.sh
+source pyretic/evaluations/scripts/nsdi16/init_settings.sh
+
+declare -a OPT_FLAGS_ARR=("-d -l")
+declare -a OPT_NAMES_ARR=("disjoint")
+declare -a TESTS=("congested_stanford" "traffic_matrix_stanford")
+CNT=5
+run_tests
+
+# remove the `exit` below to generate full table of results
+exit 
+
 declare -a OPT_FLAGS_ARR=("" "-d -l" "-d -l -i" "-d -l -i -a" "-d -l -i -a -c" "-d -l -i -a -c -b")
-# declare -a OPT_NAMES_ARR=("disjoint" "integration" "partition" "cache" "preddecomp" "fdd")
 declare -a OPT_NAMES_ARR=("noopts" "disjoint" "integration" "partition" "cache" "preddecomp")
 declare -a TESTS=("ddos_stanford" "firewall_stanford" "path_loss_stanford" "slice_stanford")
-
-SCRIPT_LOG="pyretic/evaluations/script-log.txt"
-PYCMD="/opt/pypy-2.4.0/bin/pypy"
-#PYCMD="python"
-rm -f $SCRIPT_LOG
-
-function run_tests {
 CNT=5
-for j in $(seq 1 $CNT)
-do
-
-echo '*****' Starting run $j | tee -a $SCRIPT_LOG
-for TEST in "${TESTS[@]}";
-do
-
-echo '----' | tee -a $SCRIPT_LOG
-echo "Running test" $TEST | tee -a $SCRIPT_LOG
-i=0
-DCR=1
-for OPT_FLAGS in "${OPT_FLAGS_ARR[@]}"
-do
-    echo '-----' | tee -a $SCRIPT_LOG
-    echo "Start time" | tee -a $SCRIPT_LOG
-    date | tee -a $SCRIPT_LOG
-    name=${OPT_NAMES_ARR[$i]}
-    echo sudo $PYCMD pyretic/evaluations/eval_compilation.py -t $TEST -u -r -s 16 $OPT_FLAGS -f evaluation_results/nsdi16/${TEST}_${name}_$j | tee -a $SCRIPT_LOG
-    sudo $PYCMD pyretic/evaluations/eval_compilation.py -t $TEST -u -r -s 16 $OPT_FLAGS -f evaluation_results/nsdi16/${TEST}_${name}_$j | tee -a $SCRIPT_LOG
-    echo "Done with run" $TEST $name $j | tee -a $SCRIPT_LOG
-    i=$((i + DCR))
-done # end opts loop
-
-done # end test loop
-
-done # end count loop
-}
-
 run_tests
 
 declare -a OPT_FLAGS_ARR=("-d -l -i" "-d -l -i -a" "-d -l -i -a -c" "-d -l -i -a -c -b")
 declare -a OPT_NAMES_ARR=("integration" "partition" "cache" "preddecomp")
 declare -a TESTS=("traffic_matrix_stanford" "congested_stanford" )
-
+CNT=5
 run_tests
---
+++ pyretic/evaluations/scripts/nsdi16/run_tests.sh
@@ -0,0 +1,28 @@
+function run_tests {
+for j in $(seq 1 $CNT)
+do
+
+echo '*****' Starting run $j | tee -a $SCRIPT_LOG
+for TEST in "${TESTS[@]}";
+do
+
+echo '----' | tee -a $SCRIPT_LOG
+echo "Running test" $TEST | tee -a $SCRIPT_LOG
+i=0
+DCR=1
+for OPT_FLAGS in "${OPT_FLAGS_ARR[@]}"
+do
+    echo '-----' | tee -a $SCRIPT_LOG
+    echo "Start time" | tee -a $SCRIPT_LOG
+    date | tee -a $SCRIPT_LOG
+    name=${OPT_NAMES_ARR[$i]}
+    echo sudo $PYCMD pyretic/evaluations/eval_compilation.py -t $TEST -u -r -s 16 $OPT_FLAGS -f evaluation_results/nsdi16/${TEST}_${name}_$j | tee -a $SCRIPT_LOG
+    sudo $PYCMD pyretic/evaluations/eval_compilation.py -t $TEST -u -r -s 16 $OPT_FLAGS -f evaluation_results/nsdi16/${TEST}_${name}_$j | tee -a $SCRIPT_LOG
+    echo "Done with run" $TEST $name $j | tee -a $SCRIPT_LOG
+    i=$((i + DCR))
+done # end opts loop
+
+done # end test loop
+
+done # end count loop
+}
