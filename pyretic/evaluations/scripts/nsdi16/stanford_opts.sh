609e1afccfb001e48c6d3c7a67f95f47ff3b64b9
Srinivas Narayana <ngsrinivas@gmail.com>
2015-09-10 10:32:21 -0700

    stanford no-opts and disjoint-only runs

--- pyretic/evaluations/scripts/nsdi16/stanford_opts.sh
+++ pyretic/evaluations/scripts/nsdi16/stanford_opts.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+
+source pyretic/evaluations/scripts/nsdi16/run_tests.sh
+source pyretic/evaluations/scripts/nsdi16/init_settings.sh
+
+declare -a OPT_FLAGS_ARR=("-d -l" "")
+declare -a OPT_NAMES_ARR=("disjoint" "noopts")
+declare -a TESTS=("stanford")
+CNT=5
+run_tests
+
+# remove the `exit` below to generate full table of results
+exit;
+
+# TODO: old-style code below; must refactor to use run_tests as above
 # declare -a OPT_FLAGS_ARR = ("-d -l" "-d -l -i" "-d -l -i -a" "-d -l -i -a -c" "-d -l -i -a -c -b" "-d -l -i -a -c -b --use_fdd")
-declare -a OPT_FLAGS_ARR=("-d -l" "-d -l -i" "-d -l -i -a" "-d -l -i -a -c" "-d -l -i -a -c -b")
+declare -a OPT_FLAGS_ARR=("-d -l -i" "-d -l -i -a" "-d -l -i -a -c" "-d -l -i -a -c -b")
 # declare -a OPT_NAMES_ARR=("disjoint" "integration" "partition" "cache" "preddecomp" "fdd")
-declare -a OPT_NAMES_ARR=("disjoint" "integration" "partition" "cache" "preddecomp")
+declare -a OPT_NAMES_ARR=("integration" "partition" "cache" "preddecomp")
 
 CNT=5
 for j in $(seq 1 $CNT)
