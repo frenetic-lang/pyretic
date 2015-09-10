a824e67cc2f117c9654b788a1dee4d7381350e5d
Srinivas Narayana <ngsrinivas@gmail.com>
2015-09-10 11:22:20 -0700

    refactor capture command definition to one place

--- pyretic/evaluations/scripts/nsdi16/run_tests.sh
+++ pyretic/evaluations/scripts/nsdi16/run_tests.sh
@@ -1,27 +1,28 @@
 function run_tests {
+cap_cmd="tee -a $SCRIPT_LOG"
 for j in $(seq 1 $CNT)
 do
 
-echo '*****' Starting run $j | tee -a $SCRIPT_LOG
+echo '*****' Starting run $j | $cap_cmd
 for TEST in "${TESTS[@]}";
 do
 
-echo '----' | tee -a $SCRIPT_LOG
-echo "Running test" $TEST | tee -a $SCRIPT_LOG
+echo '----' | $cap_cmd
+echo "Running test" $TEST | $cap_cmd
 i=0
 DCR=1
 for OPT_FLAGS in "${OPT_FLAGS_ARR[@]}"
 do
-    echo '-----' | tee -a $SCRIPT_LOG
-    echo "Start time" | tee -a $SCRIPT_LOG
-    date | tee -a $SCRIPT_LOG
+    echo '-----' | $cap_cmd
+    echo "Start time" | $cap_cmd
+    date | $cap_cmd
     name=${OPT_NAMES_ARR[$i]}
-    cmd="sudo timeout $RUN_TIMEOUT $PYCMD pyretic/evaluations/eval_compilation.py -t $TEST -u -r -s 16 $OPT_FLAGS -f evaluation_results/nsdi16/${TEST}_${name}_$j | tee -a $SCRIPT_LOG"
-    echo $cmd
-    $cmd
-    echo "Done with run" $TEST $name $j | tee -a $SCRIPT_LOG
-    echo "End time" | tee -a $SCRIPT_LOG
-    date | tee -a $SCRIPT_LOG
+    run_cmd="sudo timeout $RUN_TIMEOUT $PYCMD pyretic/evaluations/eval_compilation.py -t $TEST -u -r -s 16 $OPT_FLAGS -f evaluation_results/nsdi16/${TEST}_${name}_$j"
+    echo $run_cmd | $cap_cmd
+    $run_cmd | $cap_cmd
+    echo "Done with run" $TEST $name $j | $cap_cmd
+    echo "End time" | $cap_cmd
+    date | $cap_cmd
     i=$((i + DCR))
 done # end opts loop
 
