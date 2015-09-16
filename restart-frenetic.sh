34754d997f9ad9a7f97e56835adc13b1fa5ff837
Srinivas Narayana <ngsrinivas@gmail.com>
2015-09-16 15:24:09 -0700

    invoke frenetic in a way suitable both for old and new compile-servers

--- restart-frenetic.sh
+++ restart-frenetic.sh
@@ -5,4 +5,4 @@ if [ `ps ax | grep 'frenetic compile-server' | grep -v grep | wc -l` -gt 0  ];
 then
     kill `ps ax | grep 'frenetic compile-server' | grep -v grep | awk '{print $1}'`
 fi
-./frenetic compile-server --verbosity=error
+./frenetic compile-server --verbosity error
--- start-frenetic.sh
+++ start-frenetic.sh
@@ -2,5 +2,5 @@
 cd $HOME/pyretic
 # Check if frenetic compile-server runs already, and start it up if it doesn't.
 if [ `ps ax | grep 'frenetic compile-server' | wc -l` -lt 2 ]; then
-    ./frenetic compile-server --verbosity=error &
+    ./frenetic compile-server --verbosity error &
 fi
