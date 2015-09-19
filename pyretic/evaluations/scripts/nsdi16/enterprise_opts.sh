26a62d7ece9f6b15cf46bb87e5495caf20d0197e
Srinivas Narayana <ngsrinivas@gmail.com>
2015-09-19 14:21:20 -0700

    fix berkeley topology size in tests

--- pyretic/evaluations/scripts/nsdi16/enterprise_opts.sh
+++ pyretic/evaluations/scripts/nsdi16/enterprise_opts.sh
@@ -6,6 +6,6 @@ source pyretic/evaluations/scripts/nsdi16/init_settings.sh
 declare -a OPT_FLAGS_ARR=("-d -l -i -a -c -b" "-d -l -i -a -c -b --use_fdd")
 declare -a OPT_NAMES_ARR=("preddecomp" "fdd")
 declare -a TESTS=("berkley" "purdue" "rf3257" "rf6461")
-declare -a NUM_NODES_ARR=("23" "98" "161" "138")
+declare -a NUM_NODES_ARR=("25" "98" "161" "138")
 CNT=5
 run_tests
