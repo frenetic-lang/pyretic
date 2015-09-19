e5459af02f6a0c2e35262894978fc530f26f7dc6
Srinivas Narayana <ngsrinivas@gmail.com>
2015-09-19 14:08:49 -0700

    evaluation scripts for other enterprise networks

--- pyretic/evaluations/scripts/nsdi16/init_settings.sh
+++ pyretic/evaluations/scripts/nsdi16/init_settings.sh
@@ -2,4 +2,4 @@ SCRIPT_LOG="pyretic/evaluations/log-evals.txt"
 rm -f $SCRIPT_LOG
 PYCMD="/opt/pypy-2.4.0/bin/pypy"
 #PYCMD="python"
-RUN_TIMEOUT="2h"
+RUN_TIMEOUT="2.5h"
---
+++ pyretic/evaluations/scripts/nsdi16/run_enterprise_tests.sh
@@ -0,0 +1,37 @@
+function run_tests {
+cap_cmd="tee -a $SCRIPT_LOG"
+for j in $(seq 1 $CNT)
+do
+
+nodeindex=0
+INCR=1
+echo '*****' Starting run $j | $cap_cmd
+for TEST in "${TESTS[@]}";
+do
+
+NUM_NODES=${NUM_NODES_ARR[$nodeindex]}
+echo '----' | $cap_cmd
+echo "Running test" $TEST | $cap_cmd
+i=0
+DCR=1
+for OPT_FLAGS in "${OPT_FLAGS_ARR[@]}"
+do
+    echo '-----' | $cap_cmd
+    echo "Start time" | $cap_cmd
+    date | $cap_cmd
+    name=${OPT_NAMES_ARR[$i]}
+    run_cmd="sudo timeout $RUN_TIMEOUT $PYCMD pyretic/evaluations/eval_compilation.py -t $TEST -u -r -s $NUM_NODES $OPT_FLAGS -f evaluation_results/nsdi16/${TEST}_${name}_$j"
+    echo $run_cmd | $cap_cmd
+    $run_cmd 2>&1 | $cap_cmd
+    dmesg | tail -4 | $cap_cmd
+    echo "Done with run" $TEST $name $j | $cap_cmd
+    echo "End time" | $cap_cmd
+    date | $cap_cmd
+    i=$((i + DCR))
+done # end opts loop
+
+nodeindex=$((nodeindex + INCR))
+done # end test loop
+
+done # end count loop
+}
