4a0dab9d8aaa82f8fa53b96d7c83b327887aeb28
Srinivas Narayana <ngsrinivas@gmail.com>
2015-09-23 22:21:50 -0700

    parameterized waxman graphs generated

---
+++ pyretic/evaluations/gen-waxman.py
@@ -0,0 +1,42 @@
+""" Generate connected waxman topologies.
+
+Author: Srinivas Narayana (narayana@cs.princeton.edu)
+
+Run as:
+
+python gen-waxman.py <node-count> <alpha> <beta>
+
+To generate topologies over a range of node counts, you could use a bash loop,
+like
+
+alpha=0.4; beta=0.2; for i in `echo 20 40 60 80 100 120 140 160 180 200`; do mkdir -p pyretic/evaluations/Tests/waxman-${alpha}-${beta}/$i ; python pyretic/evaluations/gen-waxman.py $i $alpha $beta > pyretc/evaluations/Tests/waxman-${alpha}-${beta}/${i}/original-topo.txt ; done | grep FAIL
+
+"""
+
+import fnss
+import networkx as nx
+import time
+import sys
+
+n=int(sys.argv[1])
+alpha=float(sys.argv[2])
+beta=float(sys.argv[3])
+standard_bw = 100 # dummy bandwidth value to go on topo files
+
+disconnected = True
+tries = 0
+while disconnected:
+    topo = fnss.waxman_1_topology(n, alpha=alpha, beta=beta)
+    disconnected = not nx.is_connected(topo)
+    tries += 1
+    if tries == 300:
+        sys.stderr.write("%d FAIL\n" % n)
+        break
+
+if not disconnected:
+    print "edges"
+    for n in topo.nodes():
+        print n, 'p%d' % n
+    print "links"
+    for (src,dst) in topo.edges():
+        print src, dst, standard_bw
