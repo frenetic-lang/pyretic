2f9e543c71baf03e788e3b877a6f8fd750a43573
Srinivas Narayana <ngsrinivas@gmail.com>
2015-09-23 23:15:23 -0700

    modify topo files to include only 70% edges

--- pyretic/evaluations/gen-waxman.py
+++ pyretic/evaluations/gen-waxman.py
@@ -11,6 +11,8 @@ like
 
 alpha=0.4; beta=0.2; for i in `echo 20 40 60 80 100 120 140 160 180 200`; do mkdir -p pyretic/evaluations/Tests/waxman-${alpha}-${beta}/$i ; python pyretic/evaluations/gen-waxman.py $i $alpha $beta > pyretc/evaluations/Tests/waxman-${alpha}-${beta}/${i}/original-topo.txt ; done | grep FAIL
 
+alpha=0.4; beta=0.2; for i in `echo 20 40 60 80 100 120 140 160 180 200`; do python pyretic/evaluations/rw_sertopo.py pyretic/evaluations/Tests/waxman-${alpha}-${beta}/${i}/original-topo.txt > pyretic/evaluations/Tests/waxman-${alpha}-${beta}/${i}/topo.txt ; done
+
 """
 
 import fnss
@@ -35,7 +37,9 @@ while disconnected:
 
 if not disconnected:
     print "edges"
-    for n in topo.nodes():
+    nodes = list(topo.nodes())
+    num_nodes = len(nodes)
+    for n in nodes[:int(0.7*num_nodes)]:
         print n, 'p%d' % n
     print "links"
     for (src,dst) in topo.edges():
