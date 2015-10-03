97ba3b46e53a4e2f8b000d5a87d4ff25b08cff95
Srinivas Narayana <ngsrinivas@gmail.com>
2015-10-03 11:15:45 -0700

    remove purdue enterprise evaluation

--- pyretic/evaluations/scripts/nsdi16/enterprise_opts.sh
+++ pyretic/evaluations/scripts/nsdi16/enterprise_opts.sh
@@ -5,7 +5,9 @@ source pyretic/evaluations/scripts/nsdi16/init_settings.sh
 
 declare -a QUERY_FLAGS_ARR=("q1 tm" "q1 congested_link" "q1 ddos" "q1 slice" "q1 path_loss" "q1 firewall")
 declare -a QUERY_NAMES_ARR=("tm" "congested_link" "ddos" "slice" "path_loss" "firewall")
-declare -a TESTS=("berkley" "purdue" "rf1755")
+# remove purdue from tested enterprises; data not publicly available
+# declare -a TESTS=("berkley" "purdue" "rf1755")
+declare -a TESTS=("berkley" "rf1755")
 declare -a NUM_NODES_ARR=("25" "98" "87")
 declare -a OPT_FLAGS="-d -l -i -a -c -b --use_fdd"
 declare -a OPT_NAME="fdd"
